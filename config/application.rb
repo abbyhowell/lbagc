161e892998eb0ddd8751c635c74d132f6d5c7178
Owen Williams <owen-github@ywwg.com>
2026-01-24 13:30:40 -0500

    Update event year from 2025 to 2026

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Ffagc
     }
 
     # The year that the event will take place
-    config.event_year = "2025"
+    config.event_year = "2026"
     # The timezone for the event, used to calculate submission and voting
     # deadlines.
     config.event_timezone = "America/New_York"
